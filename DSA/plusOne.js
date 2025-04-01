6de2e4679db48a124b03202f936b48f801bc28cf
Anshul-cu07 <savansharma22@gmail.com>
2025-04-01 11:02:42 +0530

    plus one

---
+++ DSA/plusOne.js
@@ -0,0 +1,32 @@
+//case 1:
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+// case 2:
+
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
+
+// let digits = [1, 2, 3];
+let digits = [9];
+function plusOne(digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] !== 9) {
+      digits[i]++;
+      console.log(digits);
+      break;
+    } else {
+      digits[i] = 0;
+    }
+  }
+  digits.unshift(1);
+  console.log(digits);
+}
+
+plusOne(digits);
